e9fc96555a970722ba8dd5ffb6004a0f83530e2a
Aaron Wilder <element1028@gmail.com>
2018-04-26 20:43:41 -0700

    cleaning up repos (old uncommitted changes)

--- server.js
+++ server.js
@@ -1,3 +1,9 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const knex =require*"./database/db.js"
+const knex = require("./database/db.js")
+
+const server = express();
+server.use(bodyParser.json());
+server.get('/', (req,res) => {
+  res.status(200).json("It be Poopen yo!")
+})
